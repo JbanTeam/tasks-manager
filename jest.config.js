333bd2f6efb61543abca05ea73b979c566dbb60d
Vital Alex <jbanteam@yandex.ru>
2025-05-23 19:44:36 +0300

    chore: update project configuration files

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,27 @@
-module.exports = {
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config = {
   preset: 'ts-jest',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
   testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/../' }),
+  setupFiles: ['dotenv/config'],
 };
+
+export default {
+  projects: [
+    {
+      displayName: 'Unit',
+      rootDir: 'src',
+      testMatch: ['<rootDir>/**/tests/**/*.test.ts'],
+      coverageDirectory: './coverage',
+      ...config,
+    },
+  ],
+};
+
