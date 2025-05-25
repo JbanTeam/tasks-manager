7818bb610e8debfded818846fe093e1cd592301a
Vital Alex <jbanteam@yandex.ru>
2025-05-25 12:56:56 +0300

    feat(routes): add catchAsync utility to handle async route handlers

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
 
 import authMiddleware from '../middlewares/auth';
-import { getAllUsers, signUp, getUser, signIn, getDeveloperTime } from '../controllers/userController';
 import { assignTaskToUser, changeTaskStatus, addTaskToProject, deleteTaskFromDb } from '../controllers/taskController';
+import {
+  getAllUsers,
+  signUp,
+  getUser,
+  signIn,
+  getDeveloperTime,
+  logout,
+  updateAccessToken,
+} from '../controllers/userController';
 import {
   addUserToProject,
   deleteProjectFromDb,
@@ -12,27 +20,30 @@ import {
   initProject,
   removeUserFromProject,
 } from '../controllers/projectController';
+import { catchAsync } from '@src/utils/catchAsync';
 
 const routes = (router: Router) => {
-  router.post('/signup', signUp);
-  router.post('/signin', signIn);
-  router.get('/users', getAllUsers);
-  router.get('/users/:userId', getUser);
-  router.get('/users/:devId/time', authMiddleware, getDeveloperTime);
+  router.post('/signup', catchAsync(signUp));
+  router.post('/signin', catchAsync(signIn));
+  router.patch('/logout', authMiddleware, catchAsync(logout));
+  router.post('/update-access', catchAsync(updateAccessToken));
+  router.get('/users', catchAsync(getAllUsers));
+  router.get('/users/:userId', catchAsync(getUser));
+  router.get('/users/:devId/time', authMiddleware, catchAsync(getDeveloperTime));
 
-  router.get('/projects', getAllProjects);
-  router.get('/projects/own', authMiddleware, getProjectsByUser);
-  router.get('/projects/:projectId', authMiddleware, getProjectsByUser);
-  router.get('/projects/:projectId/time', authMiddleware, getProjectTime);
-  router.post('/projects', authMiddleware, initProject);
-  router.patch('/projects/:projectId/add-user', authMiddleware, addUserToProject);
-  router.patch('/projects/:projectId/remove-user', authMiddleware, removeUserFromProject);
-  router.delete('/projects/:projectId', authMiddleware, deleteProjectFromDb);
+  router.get('/projects', catchAsync(getAllProjects));
+  router.get('/projects/own', authMiddleware, catchAsync(getProjectsByUser));
+  router.get('/projects/:projectId', authMiddleware, catchAsync(getProjectsByUser));
+  router.get('/projects/:projectId/time', authMiddleware, catchAsync(getProjectTime));
+  router.post('/projects', authMiddleware, catchAsync(initProject));
+  router.patch('/projects/:projectId/add-user', authMiddleware, catchAsync(addUserToProject));
+  router.patch('/projects/:projectId/remove-user', authMiddleware, catchAsync(removeUserFromProject));
+  router.delete('/projects/:projectId', authMiddleware, catchAsync(deleteProjectFromDb));
 
-  router.post('/projects/:projectId/tasks', authMiddleware, addTaskToProject);
-  router.patch('/projects/:projectId/tasks/:taskId/assign', authMiddleware, assignTaskToUser);
-  router.patch('/projects/:projectId/tasks/:taskId/status', authMiddleware, changeTaskStatus);
-  router.delete('/projects/:projectId/tasks/:taskId', authMiddleware, deleteTaskFromDb);
+  router.post('/projects/:projectId/tasks', authMiddleware, catchAsync(addTaskToProject));
+  router.patch('/projects/:projectId/tasks/:taskId/assign', authMiddleware, catchAsync(assignTaskToUser));
+  router.patch('/projects/:projectId/tasks/:taskId/status', authMiddleware, catchAsync(changeTaskStatus));
+  router.delete('/projects/:projectId/tasks/:taskId', authMiddleware, catchAsync(deleteTaskFromDb));
 
   return router;
 };
